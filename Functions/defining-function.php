230db2b93820efac1cefb04ac500343a7780f6c3
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-08-09 23:08:35 -0500

    learning PHP defining functions with codecademy

---
+++ Functions/defining-function.php
@@ -0,0 +1,21 @@
+<?php
+//1.
+//Define a function named praisePHP. You can leave the function body blank for now.
+//
+//Checkpoint 2 Passed
+//
+//Stuck? Get a hint
+//2.
+//Add at least one echo statement inside your function body which prints a string praising the PHP language.
+//
+//Checkpoint 3 Passed
+//
+//Stuck? Get a hint
+//3.
+//Now that your function is fully defined, run your program. Notice what happens… or really what doesn’t happen.
+
+function praisePHP(){
+    echo "PHP is cool";
+}
+
+praisePHP();
