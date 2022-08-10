82f1d8a8bde6aa36a612b636a1a74f80d6ef6790
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-08-09 23:11:34 -0500

    learning PHP invoking functions with codecademy

---
+++ Functions/invoke-function.php
@@ -0,0 +1,21 @@
+<?php
+//1.
+//Define the function inflateEgo() which prints a compliment.
+//
+//Checkpoint 2 Passed
+//
+//Stuck? Get a hint
+//2.
+//Invoke your function!
+//
+//Checkpoint 3 Passed
+//
+//Stuck? Get a hint
+//3.
+//Just for kicks, edit your code so your function is invoked twice!
+
+function inflateEgo(){
+    echo "You're cool";
+}
+inflateEgo();
+inflateEgo();
