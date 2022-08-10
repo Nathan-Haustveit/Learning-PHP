a83b50fe9c17637240cec7197b4b81eb84f70a26
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-08-09 23:29:39 -0500

    learning PHP returning null with codecademy

---
+++ Functions/returning-null.php
@@ -0,0 +1,16 @@
+<?php
+//1.
+//Write a function createVacuum() which returns nothing.
+//
+//Checkpoint 2 Passed
+//
+//Stuck? Get a hint
+//2.
+//NULL can be really quirky. You don’t need to worry about the details now. But, just for fun, uncomment the line we provided at the bottom of the code. Predict what it will output to the terminal. When you have a guess, run the code to see.
+
+function createVacuum(){
+
+}
+
+echo createVacuum() * 10;
+//returns 0;
