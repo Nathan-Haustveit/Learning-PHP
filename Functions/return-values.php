bd556360553b9fa11eff809081ce15d2ce9dad24
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-08-09 23:25:27 -0500

    learning PHP return values with codecademy

---
+++ Functions/return-values.php
@@ -0,0 +1,22 @@
+<?php
+//Use echo to print the return values of each of the three provided functions in order. The challenge: you may not use more than one line of code or more than a single statement (use only one semicolon).
+//
+//Note: You can add space, new line, or other characters in between the return values.
+
+function first()
+{
+    return "You did it!\n";
+}
+
+function second()
+{
+    return "You're amazing!\n";
+}
+
+function third()
+{
+    return "You're a coding hero!\n";
+}
+
+// Write your code below:
+echo first(), second(), third();
