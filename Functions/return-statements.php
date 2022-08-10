7432c3ef6cea2b17b2243438b7e49bbe1f426101
Nathan Haustveit <nathanhaustveit@gmail.com>
2022-08-09 23:17:34 -0500

    learning PHP return statements with codecademy

---
+++ Functions/return-statements.php
@@ -0,0 +1,24 @@
+<?php
+//1.
+//Write a function printStringReturnNumber() which prints a string and returns a number value.
+//
+//Checkpoint 2 Passed
+//
+//Stuck? Get a hint
+//2.
+//Capture your function’s return value in a variable named $my_num.
+//
+//Checkpoint 3 Passed
+//
+//Stuck? Get a hint
+//3.
+//Use echo to print your $my_num variable.
+
+function printStringReturnNumber(){
+    echo "one";
+    return 1;
+}
+
+$my_num = printStringReturnNumber();
+
+echo $my_num;
